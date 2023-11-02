6645d368efbc19196199f4a9b9f186fd8ffaff7d
Phil-Shepard <Philipp-74@yandex.ru>
2023-11-02 21:07:31 +0500

    Сделал бота в телеграме

--- src/main/java/ru/shamonin/Main.java
+++ src/main/java/ru/shamonin/Main.java
@@ -1,5 +1,13 @@
-package ru.shamonin;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package ru.shamonin;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.shamonin.bot.TelegramBot;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBot = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBot.registerBot(new TelegramBot());
     }
 }
